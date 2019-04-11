2d7003c41fd80219cf35709c0f3ef1c1d6a5c848
Cristiana Yasuda <cristiana.yasuda@gmail.com>
2019-04-11 17:09:31 -0300

    arrumando o hash do multer e adicionando box id do file

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,7 +1,10 @@
+const Box = require('../models/Box');
 const File = require('../models/File');
 
 class FileController {
   async store(req, res) {
+    const box = await Box.findById(req.params.id);
+
     const file = await File.create({
       title: req.file.originalname,
       path: req.file.key
