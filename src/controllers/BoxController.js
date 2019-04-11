d8f6cb8097f2e9c6a2be2c4e7bace54ff355205d
Cristiana Yasuda <cristiana.yasuda@gmail.com>
2019-04-11 15:25:28 -0300

    arrumando a rota do boxcontroller

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -3,15 +3,16 @@ const Box = require('../models/Box');
 class BoxController {
   async store(req, res) {
     const box = await Box.create(req.body);
-// esse await esta na frente porque ele é assincrono.
+    // esse await esta na frente porque ele é assincrono.
     return res.json(box);
-  }/* store é tipo um middleware que usuário pode criar outras pastas para upload */
+  }
+  // store é tipo um middleware que usuário pode criar outras pastas para upload.
   async show(req, res) {
     const box = await Box.findById(req.params.id).populate({
       path: 'files',
       options: { sort: { createdAt: -1 } }
-    })
-    return res.json(box)
+    });
+    return res.json(box);
   }
 }
 
--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,12 +1,7 @@
-const Box = require('../models/Box');
 const File = require('../models/File');
 
 class FileController {
   async store(req, res) {
-    const box = await Box.findById(req.params.id).populate({
-        path: 'files',
-        options: { sort: { createdAt: -1 }}
-    });
     const file = await File.create({
       title: req.file.originalname,
       path: req.file.key
